84ba4fd48e06d6301e933ef846530c860cb14b2c
Marcin Religa <marcin@religa.net>
2022-06-30 11:16:04 +0200

    replace dependency on Alamofire with simple native code

---
+++ DemoApp/LoginDemo/Utility/UIImageView+imageFromServerURL.swift
@@ -0,0 +1,39 @@
+//
+//  UIImageView+imageFromServerURL.swift
+//  LoginDemo
+//
+//  Created by Marcin Religa on 30/6/22.
+//  Copyright © 2022 Intelligent Apps GmbH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func imageFromServerURL(_ URLString: String, placeHolder: UIImage?) {
+        self.image = nil
+
+        //If imageurl's imagename has space then this line going to work for this
+        let imageServerUrl = URLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+
+        guard let url = URL(string: imageServerUrl) else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+            if error != nil {
+                DispatchQueue.main.async {
+                    self.image = placeHolder
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                if let data = data {
+                    if let downloadedImage = UIImage(data: data) {
+                        self.image = downloadedImage
+                    }
+                }
+            }
+        }).resume()
+    }
+}
--- DemoApp/Pods/Target Support Files/Pods-LoginDemo/Pods-LoginDemo-frameworks.sh
+++ DemoApp/Pods/Target Support Files/Pods-LoginDemo/Pods-LoginDemo-frameworks.sh
@@ -176,8 +176,6 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxFeedback/RxFeedback.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxGesture/RxGesture.framework"
@@ -189,8 +187,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxFeedback/RxFeedback.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxGesture/RxGesture.framework"
